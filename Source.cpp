26ad7c50a537be1869142f85e5514fd747fc1476
ogatyan1019 <39644273+ogatyan1019@users.noreply.github.com>
2019-01-07 00:08:24 +0900

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -50,19 +50,50 @@ void put_data(int best)	//今回の時間更新
 int game(void)	//文章表示とスコア計算
 {
 	int count = 0, Linecount = 1,score=0;
-	int anser, CorrectAns;
+	int answer, CorrectAns,level,i;
 	FILE *fp;
 	char str[256];
 
-	printf("難易度を選択してください。\n1.準2級　2.2級　3.準1級");
+	printf("難易度を選択してください。\n1.準2級　2.2級　3.準1級\n");
+	scanf("%d", &level);
+	if ((level != 1) && (level != 2) && (level != 3))
+	{
+		for (i = 1; i <= 10; i++)
+		{
+			printf("正しい難易度を選択してください。\n準2級　2.2級　3.準1級");
+			scanf("%d", &level);
+			if ((level == 1) || (level == 2) || (level == 3))
+				break;
+			if (i = 10)
+			{
+				printf("難易度が選択されませんでした。");
+				exit (-1);
+			}
+		}
 
+	}
 	printf("これから問題が表示されます。\n選択肢の先頭の数字を入力して下さい。\n\n");
 	printf("Start!!\n");
 
-	if ((fp = fopen("Quiz.txt", "r")) == NULL)
+	if (level == 1)
 	{
-		printf("file not open !\n");
-		return -1;
+		fp = fopen("Quasi_Grade2", "r");
+	}
+
+	else if (level == 2)
+	{
+		fp = fopen("Grade2", "r");
+	}
+
+	else if (level == 3)
+	{
+		fp = fopen("Quasi_Grade1", "r");
+	}
+
+	else
+	{
+		printf("ERROR\nFile Not Open");
+		exit(-1);
 	}
 
 	while (fgets(str, 256, fp) != NULL)
@@ -71,17 +102,19 @@ int game(void)	//文章表示とスコア計算
 		if (count % 3 == 0)
 		{
 			CorrectAns = atoi(str);
-			scanf("%d", &anser);
-			if (CorrectAns == anser)
+			scanf("%d", &answer);
+			if (CorrectAns == answer)
 			{
 				printf("正解！！\n");
 				count++;
 				score++;
+				QNO++;
 			}
 			else
 			{
 				printf("残念！！");
 				printf("%s", str);
+				QNO++;
 			}
 		}
 
@@ -92,7 +125,7 @@ int game(void)	//文章表示とスコア計算
 
 		count++;
 	}
-
+	fclose(fp);
 	return(score);
 
 }
